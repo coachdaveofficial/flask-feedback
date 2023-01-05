0991045e50248578bfee1221b2a9b31333f9d89e
David Falson <david@falson.net>
2023-01-04 19:33:16 -0800

    created Feedback model

--- models.py
+++ models.py
@@ -46,3 +46,14 @@ class User(db.Model):
             return u
         else:
             return False
+
+class Feedback(db.Model):
+
+    __tablename__ = 'feedback'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, db.ForeignKey('username'))
+
+    
